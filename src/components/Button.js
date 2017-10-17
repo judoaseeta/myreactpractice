8e7383d24d7e6f2530cdebd790a1e968e8e25a9b
judoaseeta <judoaseeta@gmail.com>
2017-10-17 23:31:39 +0900

    test and styling about <Button /> is complete.

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,25 @@ import React from 'react';
 import classNames from 'classnames/bind';
 import styles from '../styles/Button.scss';
 const cx = classNames.bind(styles);
-const Button = ({children, onClick, styles, ...props}) => (
+const Button = (
+    {
+        children, 
+        isBlack,
+        isBlue, 
+        isGreen,
+        isRed, 
+        onClick, 
+        styles, 
+        ...props
+    }
+) => (
     <button
-        className={cx('button')}
+        className={cx('button', {
+            black: isBlack,
+            blue: isBlue,
+            green: isGreen,
+            red: isRed
+        })}
         onClick={onClick ? onClick : null}
         style={styles? styles : null}
         {...props}
--- src/components/__test__/Button.test.js
+++ src/components/__test__/Button.test.js
@@ -11,8 +11,21 @@ describe('<Button />',() => {
             />
         );
     })
+    it("has class '.button' ", () => {
+        const button= ButtonWrapper.find('button').first();
+        // it is expected make an error because 
+        // using css-module makes class as a hash <value className=""></value>
+        expect(button.hasClass('button')).toBe(true);
+    });
+    it("has class '.red' when set props 'isRed'", () => {
+        ButtonWrapper.setProps({
+            isRed: true
+        });
+        const button= ButtonWrapper.find('button').first();
+        expect(button.hasClass('red')).toBe(true);
+    })
     it('should invoke onClick when it is clicked', () => {
         ButtonWrapper.simulate('click');
         expect(mockOnClick).toHaveBeenCalled();
-    })
+    });
 })
